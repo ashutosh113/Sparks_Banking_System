7e85a509c3a1ea19f4f34fde31c3844cd0a968b2
ashutosh113 <ashutosh.18bit1086@abes.ac.in>
2021-10-20 00:22:53 +0530

    Banking

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+      <!-- footer part start -->
+      <footer class="py-3 my-4">
+        <p class="text-center text-muted">© 2021 Sewa Bank</p>
+      </footer>
+      <!-- footer part end -->
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- external css bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
+    <!-- <link href="./css/style.css" rel="text/stylesheet"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sewa Bank</title>
+</head>
+<body>
+
+  <!-- header with connection and sql code -->
+  <?php require_once './include/header.php' ?>
+
+
+      <!-- Carosoul part start -->
+      <section id="top" class="top">
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="images/1.jpg" class="d-block w-100 img-fluid"  alt="...">
+            <div class="carousel-caption d-none d-md-block external-img">
+              <h5>Sewa Bank</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="images/2.jfif" class="d-block w-100 img-fluid" alt="...">
+            <div class="carousel-caption d-none d-md-block external-img">
+              <h5>Express Transfer of Money.</h5>
+              <p>Transfering Money is just easy as Dialing a Phone.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="images/3.jfif" class="d-block w-100 img-fluid" alt="...">
+            <div class="carousel-caption d-none d-md-block external-img">
+              <h5>Do Banking easily.</h5>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </section>
+      <!-- CArosoul part end -->
+
+         <!-- footer  -->
+        <?php require_once './include/footer.php' ?>  
+</body>
+
+      <!-- bootstrap js script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</html>
